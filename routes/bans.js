33d9ed9d8e7f9ad57d8fe2b7e54dbc8b36af3687
c1yd3i <breslowrocky@gmail.com>
2015-05-20 14:38:28 -0400

    Eh

--- routes/bans.js
+++ routes/bans.js
@@ -17,6 +17,20 @@ router.get('/get/inactive', function(req, res, next) {
   getBans(req, res, query);
 });
 
+//POST add a ban
+/*router.post('/add', function(req, res, next) {
+  if(!req.query.steamid || !req.query.name || !req.query.time || !req.query.reason)
+    return res.json({err:'parameters not met'});
+  
+  var query = 'INSERT INTO es_bans (steamid, steamidAdmin, name, nameAdmin, serverid, unbanned, time, timeStart, reason)' +
+  'VALUES(\'' + req.query.steamid + '\', \'EXCLSERVER\', \'' + req.query.name + '\', \'EXCLSERVER\', 0, 0, ' + req.query.time + ', ' + Math.floor(new Date() / 1000) + ', \'' + req.query.reason + '\');';
+
+  db.query(query, function(err, rows) {
+   if(err) res.json(err);
+   res.json(rows);
+  });
+});*/ 
+
 function getBans(req, res, query) {
   if(req.query.name)
     query += ' AND name = \'' + req.query.name + '\'';
