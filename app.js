33f30838fdd10a4e6ed2aedb31057716e0e30677
c1yd3i <breslowrocky@gmail.com>
2015-05-20 20:31:24 -0400

    Added tempting and loading screen view, cleaned up app.js code

--- app.js
+++ app.js
@@ -2,28 +2,39 @@
 'use strict';
 
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mustacheExpress = require('mustache-express');
 
 var app = express();
 
-app.set('view engine', 'html');
+// setup view templating
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 app.set('x-powered-by', false);
 
-var index = require(__dirname + '/routes/index.js'),
-    donate = require(__dirname + '/routes/donate.js'),
+// serve static content for views
+app.use(express.static(__dirname + '/public'));
+
+// setup POST data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// setup app routes
+var donate = require(__dirname + '/routes/donate.js'),
     servers = require(__dirname + '/routes/servers.js'),
     logs = require(__dirname + '/routes/logs.js'),
     player = require(__dirname + '/routes/player.js'),
     bans = require(__dirname + '/routes/bans.js');
     
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/api/servers', servers);
 app.use('/api/donate', donate);
 app.use('/api/logs', logs);
 app.use('/api/player', player);
 app.use('/api/bans', bans);
-app.use('/api/', index);
+
+// render the loading view
+app.get('/loading', function(req, res, next){ res.render('loading', {}); });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,7 +44,7 @@ app.use(function(req, res, next) {
 });
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send(String(err.status || 500) + ": " + err.message)
+    res.send(String(err.status || 500) + ": " + err.message);
 });
 
 module.exports = app;
