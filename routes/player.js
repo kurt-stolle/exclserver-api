f0e33bec63c3a1e7b63e7666aa339d7294850b97
c1yd3i <breslowrocky@gmail.com>
2015-05-20 22:48:35 -0400

    Added ability to set a new player field in DB

--- routes/player.js
+++ routes/player.js
@@ -78,6 +78,71 @@ router.get('/:steamid/fields/:field', function(req, res, next) {
   });
 });
 
+/* POST set specific field for player provided with SteamID */
+router.post('/:steamid/fields/set', function(req, res, next) {   
+  //cannot overwrite this stuff in the DB by chance
+  if(req.body.steamid)
+    delete req.body.steamid;
+    
+  if(req.body.id)
+    delete req.body.id;
+  
+  // get all player fields for SteamID
+  db.query({
+    sql: 'SELECT * FROM `es_player_fields` WHERE `steamid` = ?',
+    values: [req.params.steamid]
+  }, 
+  function(error, results, fields) {
+    if(error) res.json(error);
+    
+    // check for POST fields that don't correspond to existing columns
+    var bodyKeys = Object.keys(req.body);
+    var columnExists = true;
+    var nonExistantColumns = [];
+    for(var i = 0; i < bodyKeys.length; i++) {
+      if (!results[0][bodyKeys[i]]) {
+        columnExists = false;
+        nonExistantColumns.push(bodyKeys[i]);
+      }
+    }
+ 
+    // add a VARCHAR(255) for all columns that do not exist 
+    if (!columnExists) {
+      db.query({
+        sql: 'ALTER TABLE `es_player_fields` ADD ?? VARCHAR(255)',
+        values: nonExistantColumns
+      }, function(error, results, fields) {
+        if(error) res.json(error);
+      });
+    }
+    
+    // if there is a result then update the existing row
+    if(results.length != 0) {
+      db.query({
+       sql: 'UPDATE `es_player_fields` SET ? WHERE `steamid` = ' + db.escape(req.params.steamid),
+       values: req.body
+      },
+      function(error, results, fields) {
+       if(error) res.json(error);
+       res.json(results);
+      });
+    
+    // if there is no result than we insert a new row
+    } else {
+      req.body.steamid = req.params.steamid;
+      
+      db.query({
+       sql: 'INSERT INTO `es_player_fields` SET ?',
+       values: req.body
+      },
+      function(error, results, fields) {
+       if(error) res.json(error);
+       res.json(results);
+      });
+    }
+  });
+});
+
 /* GET get player inventory and filter by itemtype */
 router.get('/:steamid/inventory', function(req, res, next) {
   var query = {
