9969d8f2283232c503d7c058535386d2e7e3ff98
c1yd3i <breslowrocky@gmail.com>
2015-05-27 21:01:47 -0400

    Worked on loading screen

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,20 +1,23 @@
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
-$(function() {
-  function GetServerID() {
-    return $( "body" ).data( "server-id" );
-  }
+function GetServerID() {
+  return $("body" ).data("server-id") || 15;
+}
 
-  function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
-    $('#map').html(mapname);
-    $('#maxplayers').html(maxplayers);
-    $('#steamid').html(steamid);
-  }
-
-  function SetFilesTotal( total ) {}
-
-  function DownloadingFile( fileName ) {}
-
-  function SetStatusChanged( status ) {}
-
-  function SetFilesNeeded( needed ) {}
-});
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
+  $('#steamid').text(steamid);
+  $('#mapname').text(mapname);
+  
+  $.getJSON('http://localhost:3000/api/servers/status/' + GetServerID(), function(data) {
+    console.log(data);
+    $('#players').text(data.players.length + '/' + data.maxplayers);
+    $('#ip').text(data.dns);
+  });
+      
+  $.getJSON('http://localhost:3000/api/player/STEAM_0:1:40236125/fields/bananas', function(data) {
+    $('#bananas').text(data.value);
+  });
+  
+  $.getJSON('http://localhost:3000/api/player/STEAM_0:1:40236125/fields/playtime', function(data) {
+    $('#time').text(data.value);
+  });
+}
