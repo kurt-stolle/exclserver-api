5702eda6245d2664a8256d974d74ad87399b6f66
Kurt Stolle <kurt@casualbananas.com>
2015-05-28 02:40:44 +0200

    ServerID

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,5 +1,9 @@
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
 $(function() {
+  function GetServerID() {
+    return $( "body" ).data( "server-id" );
+  }
+
   function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
     $('#map').html(mapname);
     $('#maxplayers').html(maxplayers);
